1d986f50afba66c3395d3ded4a9f10f1ce057ab5
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2024-08-03 23:39:46 -0500

    add initial styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,17 +2,20 @@ import { MouseEventHandler } from "react";
 
 interface ButtonProps {
   text: string;
-  variant: "primary" | "secondary";
+  textColor: string,
+  bgColor : string;
   onClick: MouseEventHandler;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, variant, onClick }) => {
-  // define a primary and secondary color
+const Button: React.FC<ButtonProps> = ({ text, textColor, bgColor, onClick }) => {
+
   return (
     <button
-      style={{ color: variant == "primary" ? "black" : "red" }}
+      type="button"
       onClick={onClick}
+      className={`m-4 text-${textColor} bg-${bgColor}-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
     >
+    
       {text}
     </button>
   );
--- src/pages/ChessTrainer/InfoDisplay.tsx
+++ src/pages/ChessTrainer/InfoDisplay.tsx
@@ -19,32 +19,51 @@ const InfoDisplay: React.FC<InfoDisplayProps> = ({
   feedbackMessage,
 }) => {
   return (
-    <div className="center">
-      <div>
-        <h3>
-          {gameErrors.length && fen !== STARTINGPOSFEN ? (
-            <div>
-              <img src={imageSrc} width={50} alt="Error Indicator" />
-              <p>{`${gameError.move} was played`}</p>
-            </div>
-          ) : null}
-        </h3>
+    <div>
+      <h3>
+        {gameErrors.length && fen !== STARTINGPOSFEN ? (
+          <div
+            className="w-4/5 h-12 flex flex-row justify-start items-center gap-5 p-2 mb-4 text-md  rounded-lg dark:bg-gray-800 dark:text-white"
+            role="alert"
+          >
+            <img src={imageSrc} width={40} alt="Error Indicator" />
+            <p>{`${gameError.move} was played`}</p>
+          </div>
+        ) : null}
+      </h3>
 
-        <div>
-          {fen !== STARTINGPOSFEN && movePlayed ? (
+      <div
+        className="w-4/5 h-12 flex flex-row justify-start items-center gap-5 p-2 mb-4 text-md  rounded-md  dark:bg-gray-800 dark:text-white"
+        role="alert"
+      >
+        {fen !== STARTINGPOSFEN && movePlayed ? (
+          <img
+            width={40}
+            src={
+              feedbackMessage.includes("is correct")
+                ? ERRORPNGMAP.Best
+                : ERRORPNGMAP.Wrong
+            }
+            alt=""
+          />
+        ) : (
+          <div className="flex justify-start items-center gap-5">
             <img
-              width={50}
+              width={40}
               src={
-                feedbackMessage.includes("is correct")
-                  ? ERRORPNGMAP.Best
-                  : ERRORPNGMAP.Wrong
+                gameError &&
+                fen != STARTINGPOSFEN &&
+                gameError.colorToPlay === "white"
+                  ? "/wK.svg"
+                  : "/bK.svg"
               }
               alt=""
             />
-          ) : null}
+            <h2>Find The Solution . . .</h2>
+          </div>
+        )}
 
-          <h3>{feedbackMessage}</h3>
-        </div>
+        <h3>{feedbackMessage}</h3>
       </div>
     </div>
   );
